17f082a5c904cdcfd20a118dd87153f7e0e92ef1
Hansen06 <2696371886@qq.com>
2018-05-23 11:05:58 +0800

    update

--- Crawler/items.py
+++ Crawler/items.py
@@ -6,6 +6,7 @@
 # http://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy import Field
 
 
 class CrawlerItem(scrapy.Item):
@@ -33,10 +34,56 @@ class NewsItem(scrapy.Item):
     classification = scrapy.Field()
     pass
 
+class CommentItem(scrapy.Item):
+    """评论相关
+    comments { content, vote, against, location, time, nick }
+    """
+    url = Field()
+    sitename = Field()
+    num_comments = Field()
+    comments = Field()
+    pass
 
-class WeiboItem(scrapy.Item):
-    author = scrapy.Field()
-    content = scrapy.Field()
-    num_forwarding = scrapy.Field()
-    num_comment = scrapy.Field()
-    num_likes = scrapy.Field()
+# 微博相关信息
+class InformationItem(scrapy.Item):
+    """ 个人信息 """
+    _id = Field()  # 用户ID
+    NickName = Field()  # 昵称
+    Gender = Field()  # 性别
+    Province = Field()  # 所在省
+    City = Field()  # 所在城市
+    BriefIntroduction = Field()  # 简介
+    Birthday = Field()  # 生日
+    Num_Tweets = Field()  # 微博数
+    Num_Follows = Field()  # 关注数
+    Num_Fans = Field()  # 粉丝数
+    SexOrientation = Field()  # 性取向
+    Sentiment = Field()  # 感情状况
+    VIPLevel = Field()  # 会员等级
+    Authentication = Field()  # 认证
+    URL = Field()  # 首页链接
+
+
+class TweetsItem(scrapy.Item):
+    """ 微博信息 """
+    _id = Field()  # 用户ID-微博ID
+    ID = Field()  # 用户ID
+    Content = Field()  # 微博内容
+    PubTime = Field()  # 发表时间
+    Co_oridinates = Field()  # 定位坐标
+    Tools = Field()  # 发表工具/平台
+    Num_Like = Field()  # 点赞数
+    Num_Comment = Field()  # 评论数
+    Transfer = Field()  # 转载数
+
+
+class FollowsItem(scrapy.Item):
+    """ 关注人列表 """
+    _id = Field()  # 用户ID
+    follows = Field()  # 关注
+
+
+class FansItem(scrapy.Item):
+    """ 粉丝列表 """
+    _id = Field()  # 用户ID
+    fans = Field()  # 粉丝
--- Crawler/pipelines.py
+++ Crawler/pipelines.py
@@ -10,7 +10,8 @@ import re
 import os
 import pymongo
 from Crawler.util import RedisFactory
-
+from Crawler.items import NewsItem, CommentItem
+from Crawler.items import TweetsItem, InformationItem, FollowsItem, FansItem
 
 class CrawlerPipeline(object):
     def __init__(self):
@@ -49,9 +50,38 @@ class CrawlerPipeline(object):
         self.file.close()
 
 
+class MongoDBPipleline(object):
+    def __init__(self):
+        clinet = pymongo.MongoClient("localhost", 27017)
+        db = clinet["Sina"]
+        self.Information = db["Information"]
+        self.Tweets = db["Tweets"]
+        self.Follows = db["Follows"]
+
+    def process_item(self, item, spider):
+        """ 判断item的类型，并作相应的处理，再入数据库 """
+        if isinstance(item, InformationItem):
+            try:
+                self.Information.insert(dict(item))
+            except Exception:
+                pass
+        elif isinstance(item, TweetsItem):
+            try:
+                self.Tweets.insert(dict(item))
+            except Exception:
+                pass
+        elif isinstance(item, FollowsItem):
+            try:
+                self.Follows.insert(dict(item))
+            except Exception:
+                pass
+        return item
+
+
 class MongoPipeline(object):
 
-    collection_name = 'all_news'
+    db_name = 'all_news'
+    db_comment = 'all_comments'
 
     def __init__(self, mongo_uri, mongo_db):
         self.mongo_uri = mongo_uri
@@ -61,7 +91,7 @@ class MongoPipeline(object):
     def from_crawler(cls, crawler):
         return cls(
             mongo_uri=crawler.settings.get('MONGO_URI'),
-            mongo_db=crawler.settings.get('MONGO_DATABASE', 'items')
+            mongo_db=crawler.settings.get('MONGO_DATABASE')
         )
 
     def open_spider(self, spider):
@@ -72,5 +102,11 @@ class MongoPipeline(object):
         self.client.close()
 
     def process_item(self, item, spider):
-        self.db[self.collection_name].insert(dict(item))
-        return item
+        if isinstance(item, NewsItem):
+            key = {'url': item['url']}
+            self.db[self.db_name].update(key, dict(item), upsert=True)
+            return item
+        elif isinstance(item, CommentItem):
+            key = {'url': item['url']}
+            self.db[self.db_comment].update(key, dict(item), upsert=True)
+            return 'comments of ' + item['url']
